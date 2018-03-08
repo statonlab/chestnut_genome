4bc81712684a3c4506076f9071df6170e3ad79dd
Meg Staton <mestato@gmail.com>
2018-03-08 13:55:02 -0500

    Starting script to get contig2LG stats

---
+++ Contig2LG_Stats.R
@@ -0,0 +1,8 @@
+### Task
+## To take the current set of contig to LG mappings
+## And print out
+## the number of contigs that map to a single LG across all maps
+## their total length
+## the number of contigs (and a list) that map to more than one LG 
+## their total length
+
