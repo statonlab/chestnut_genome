33932087b6eb21dfe7f29c39cd2a018c6bf5ff3e
jiali0411 <jyu28@vols.utk.edu>
2019-11-05 14:58:23 -0500

    updates

--- mds_plot/plot_mds.R
+++ mds_plot/plot_mds.R
@@ -1,10 +1,11 @@
 setwd("~/Desktop/Jiali/UTK/chestnut/chestnut_genome/mds_plot/")
 library(ggplot2)
 ## MDS plot using all.fitered.mds
-d <- read.table("all.filtered.mds", h=T)
+d <- read.table("plink.mds", h=T)
 d$genotype <-  gsub(":H.*","",d$FID)
-d$genotype[1:74] <- "B3F3"
-d$species = factor(c(rep("B3F3", 74), rep("Clapper", 16), rep("C.dentata", 79), rep("Vanuxem",16)))
+d$genotype[1:76] <- "B3F3"
+d <- d[-(191:192),]
+d$species = factor(c(rep("B3F3", 76), rep("Clapper", 16), rep("C.dentata", 82), rep("Vanuxem",16)))
 
 plot(d$C1, d$C2, col=as.integer(d$species), pch=19, xlab="MDS component 1", ylab="MDS component 2", main = "MDS")
 legend("topright", c("B3F3", "Clapper", "C.dentata","Vanuxem"), pch=19, col=c(1,3,2,4))
@@ -17,7 +18,7 @@ ggplot(d, aes(C1, C2, shape=species, color=genotype)) +
   scale_color_brewer(palette="Set3")+
   theme_classic(base_size = 14)
 
-ggsave("MDS_all.png", height = 8, width = 8)  
+ggsave("MDS_all_version3.2.png", height = 8, width = 8)  
 
 ## mds plot using all.taxamerge.filtered.mds
 taxa <- read.table("all.taxamerged.filtered.mds", h=T, stringsAsFactors = F)
